19019d69828d3cc6f48acd7d76b46d68dba34269
Etienne Pierre-Doray <etiennep@chromium.org>
2017-11-09 21:38:37 -0500

    Experiment jacobi solver for poisson image blending

--- main.cpp
+++ main.cpp
@@ -1,102 +1,226 @@
+#include "gil/vec.hpp"
+
+#include "gil/mat.hpp"
+#include "gil/image_file.hpp"
 #include <iostream>
 
 #include <opencv2/core/core.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
 #include <opencv2/highgui/highgui.hpp>
 
+//#include <boost/gil/gil_all.hpp>
+
+//#include "poisson.hpp"
+
 //Jacobi method
+//freeimage
 
 // z_1 + s < a, z_2 + s < b
 
-cv::Mat make_boundary(const cv::Mat& mask) {
-  cv::Mat boundary(mask.rows, mask.cols, mask.depth(), cv::Scalar(0));
-  size_t step = mask.step1();
-  for (size_t i = 0; i < mask.rows-1; ++i) {
-    const uint8_t* mask_it = mask.template ptr<uint8_t>(i);
-    uint8_t* bound_it = boundary.template ptr<uint8_t>(i);
-    for (size_t j = 0; j < mask.cols-1; ++j, ++mask_it, ++bound_it) {
-      if (bool(*mask_it > 128) != bool(mask_it[1] > 128)) {
+
+gil::mat<uint8_t> make_boundary(gil::mat_cview<uint8_t> mask) {
+  gil::mat<uint8_t> boundary({mask.rows(), mask.cols()});
+  size_t step = mask.stride();
+  for (int i = 0; i < mask.rows()-1; ++i) {
+    auto mask_it = mask.row_cbegin(i);
+    auto bound_it = boundary.row_begin(i);
+    for (size_t j = 0; j < mask.cols()-1; ++j, ++mask_it, ++bound_it) {
+      if (*mask_it >= 128 && mask_it[1] < 128)
         *bound_it = 255;
+      if (*mask_it < 128 && mask_it[1] >= 128)
         bound_it[1] = 255;
-      }
-      if (bool(*mask_it > 128) != bool(mask_it[step] > 128)) {
+      if (*mask_it >= 128 && mask_it[step] < 128)
         *bound_it = 255;
+      if (*mask_it < 128 && mask_it[step] >= 128)
         bound_it[step] = 255;
-      }
     }
   }
   return boundary;
 }
 
-cv::Mat make_target(const cv::Mat& f, const cv::Mat& g, const cv::Mat& mask, const cv::Mat& boundary) {
-  assert(f.size == mask.size);
-  assert(g.size == mask.size);
-  assert(boundary.size == mask.size);
-  cv::Mat dst(f.rows, f.cols, f.depth(), cv::Scalar(0));
-  size_t step = dst.step1();
-  for (size_t i = 1; i < mask.rows-1; ++i) {
-    const uint8_t* mask_it = mask.template ptr<uint8_t>(i);
-    const uint8_t* bound_it = boundary.template ptr<uint8_t>(i);
-    const cv::Vec3b* f_it = f.template ptr<cv::Vec3b>(i);
-    const cv::Vec3b* g_it = g.template ptr<cv::Vec3b>(i);
-    cv::Vec3b* dst_it = dst.template ptr<cv::Vec3b>(i);
-    for (size_t j = 1; j < mask.cols-1; ++j, ++mask_it, ++bound_it, ++f_it, ++g_it, ++dst_it) {
-      auto value = *g_it;
-      if (*bound_it == 0) {
-        value += *f_it;
+gil::mat<gil::vec3f> make_guidance(gil::mat_cview<gil::vec3f> f, gil::mat_cview<gil::vec3f> g, gil::mat_cview<uint8_t> mask, gil::mat_cview<uint8_t> boundary) {
+  assert(f.size() == mask.size());
+  assert(g.size() == mask.size());
+  assert(boundary.size() == mask.size());
+  gil::mat<gil::vec3f> dst(f.size());
+  size_t g_step = g.stride();
+  size_t dst_step = dst.stride();
+  for (int i = 1; i < mask.rows()-1; ++i) {
+    auto mask_it = mask.row_cbegin(i)+1;
+    auto bound_it = boundary.row_cbegin(i)+1;
+    auto f_it = f.row_cbegin(i)+1;
+    auto g_it = g.row_cbegin(i)+1;
+    auto dst_it = dst.row_begin(i)+1;
+    for (size_t j = 1; j < mask.cols()-1; ++j, ++mask_it, ++bound_it, ++f_it, ++g_it, ++dst_it) {
+      if (*mask_it < 128) {
+        *dst_it += 4.0**g_it - (g_it[-1] + g_it[1] + g_it[-g_step] + g_it[g_step]);
       }
-      dst_it[-step] += value;
-      dst_it[step] += value;
-      dst_it[-1] += value;
-      dst_it[1] += value;
-      *dst_it -= 4*(*g_it);
+      if (*bound_it == 255) {
+        dst_it[-1] += *f_it;
+        dst_it[1] += *f_it;
+        dst_it[-dst_step] += *f_it;
+        dst_it[dst_step] += *f_it;
+      }
     }
   }
   return dst;
 }
 
-void copy(const cv::Mat& src, const cv::Mat& mask, cv::Mat dst) {
-  assert(src.size == mask.size);
-  assert(dst.size == mask.size);
-  assert(mask.depth() == CV_8U);
+gil::mat<gil::vec3f> make_guidance2(gil::mat_cview<gil::vec3f> f, gil::mat_cview<gil::vec3f> g, gil::mat_cview<uint8_t> mask, gil::mat_cview<uint8_t> boundary) {
+  assert(f.size() == mask.size());
+  assert(g.size() == mask.size());
+  assert(boundary.size() == mask.size());
+  gil::mat<gil::vec3f> dst(f.size());
+  size_t g_step = g.stride();
+  size_t f_step = f.stride();
+  size_t dst_step = dst.stride();
+  for (int i = 1; i < mask.rows()-1; ++i) {
+    auto mask_it = mask.row_cbegin(i)+1;
+    auto bound_it = boundary.row_cbegin(i)+1;
+    auto f_it = f.row_cbegin(i)+1;
+    auto g_it = g.row_cbegin(i)+1;
+    auto dst_it = dst.row_begin(i)+1;
+    for (size_t j = 1; j < mask.cols()-1; ++j, ++mask_it, ++bound_it, ++f_it, ++g_it, ++dst_it) {
+      if (*mask_it < 128) {
+        *dst_it += std::max(*g_it - g_it[-1], *f_it - f_it[-1]);
+        *dst_it += std::max(*g_it - g_it[1], *f_it - f_it[-1]);
+        *dst_it += std::max(*g_it - g_it[-g_step], *f_it - f_it[-f_step]);
+        *dst_it += std::max(*g_it - g_it[g_step], *f_it - f_it[f_step]);
+      }
+      if (*bound_it == 255) {
+        dst_it[-1] += *f_it;
+        dst_it[1] += *f_it;
+        dst_it[-dst_step] += *f_it;
+        dst_it[dst_step] += *f_it;
+      }
+    }
+  }
+  return dst;
+}
+
+template <class T>
+void apply_mask(gil::mat_cview<uint8_t> mask, gil::mat_view<T> f) {
+  for (int i = 0; i < f.rows(); ++i) {
+    auto f_it = f.row_begin(i);
+    const uint8_t* mask_it = mask.row_begin(i);
+    for (size_t j = 0; j < f.cols(); ++j, ++f_it, ++mask_it) {
+      if (*mask_it >= 128) {
+        *f_it = {};
+      }
+    }
+  }
+}
+
+void jacobi_iteration(gil::mat_cview<gil::vec3f> src, gil::mat_cview<gil::vec3f> b, gil::mat_cview<uint8_t> mask, gil::mat_view<gil::vec3f> dst) {
+  assert(src.size() == mask.size());
+  assert(b.size() == mask.size());
+  assert(dst.size() == mask.size());
+  size_t src_step = src.stride();
+  for (int i = 1; i < src.rows()-1; ++i) {
+    const gil::vec3f* src_it = src.row_cbegin(i)+1;
+    const gil::vec3f* b_it = b.row_cbegin(i)+1;
+    const uint8_t* mask_it = mask.row_cbegin(i)+1;
+    gil::vec3f* dst_it = dst.row_begin(i)+1;
+    for (size_t j = 1; j < src.cols()-1; ++j, ++src_it, ++dst_it, ++b_it, ++mask_it) {
+      if (*mask_it < 128) {
+        *dst_it = (*b_it + src_it[-1] + src_it[1] + src_it[-src_step] + src_it[src_step]) / 4.0;
+      }
+    }
+  }
+}
+
+void apply_remainder(gil::mat_cview<gil::vec3f> src, gil::mat_cview<uint8_t> mask, gil::mat_view<gil::vec3f> dst) {
+  size_t src_step = src.stride();
+  for (int i = 1; i < src.rows()-1; ++i) {
+    const gil::vec3f* src_it = src.row_cbegin(i)+1;
+    const uint8_t* mask_it = mask.row_cbegin(i)+1;
+    gil::vec3f* dst_it = dst.row_begin(i)+1;
+    for (size_t j = 1; j < src.cols()-1; ++j, ++src_it, ++dst_it, ++mask_it) {
+      if (*mask_it < 128) {
+        *dst_it = (src_it[-1] + src_it[1] + src_it[-src_step] + src_it[src_step]);
+      }
+    }
+  }
+}
+
+void copy(gil::mat_cview<gil::vec3f> src, gil::mat_cview<uint8_t> mask, gil::mat_view<gil::vec3f> dst) {
+  assert(src.size() == mask.size());
+  assert(dst.size() == mask.size());
   
-  for (size_t i = 0; i < mask.rows; ++i) {
-    const uint8_t* mask_it = mask.template ptr<uint8_t>(i);
-    const cv::Vec3b* src_it = src.template ptr<cv::Vec3b>(i);
-    cv::Vec3b* dst_it = dst.template ptr<cv::Vec3b>(i);
-    for (size_t j = 0; j < mask.cols; ++j, ++mask_it, ++src_it, ++dst_it) {
-      if (*mask_it == 0) {
+  for (int i = 0; i < mask.rows(); ++i) {
+    const uint8_t* mask_it = mask.row_cbegin(i);
+    const gil::vec3f* src_it = src.row_cbegin(i);
+    gil::vec3f* dst_it = dst.row_begin(i);
+    for (size_t j = 0; j < mask.cols(); ++j, ++mask_it, ++src_it, ++dst_it) {
+      if (*mask_it < 128) {
         *dst_it = *src_it;
       }
     }
   }
-  
-  //src.copyTo(dst);
+}
+
+template <class T>
+struct cv_channel {};
+
+template <>
+struct cv_channel<uint8_t> { static constexpr int value = CV_8U; };
+
+template <>
+struct cv_channel<gil::vec3b> { static constexpr int value = CV_8UC3; };
+
+template <>
+struct cv_channel<gil::vec3f> { static constexpr int value = CV_32FC3; };
+
+template <class T>
+void display(gil::mat_view<T> im, gil::vec2<size_t> size) {
+  cv::namedWindow("Display window", cv::WINDOW_NORMAL); // Create a window for display.
+  auto im_cv = cv::Mat(int(im.rows()), int(im.cols()), cv_channel<T>::value, im.data(), im.stride() * sizeof(T));
+  cv::Mat tmp;
+  cv::resize(im_cv, tmp, cv::Size(int(size[1]), int(size[0])));
+  cv::imshow("Display window", tmp);  // Show our image inside it.
+  cv::resizeWindow("Display window", int(size[1]), int(size[0]));
+
+  cv::waitKey(0); // Wait for a keystroke in the window
 }
 
 int main() {
-  cv::Mat cat = cv::imread("cat.jpg", CV_LOAD_IMAGE_COLOR); // Read the file
-  cv::Mat dog = cv::imread("dog.jpg", CV_LOAD_IMAGE_COLOR); // Read the file
-  cv::Mat mask = cv::imread("mask.jpg", CV_LOAD_IMAGE_COLOR); // Read the file
+  using namespace std::placeholders;
   
-  cv::cvtColor(mask, mask, cv::COLOR_RGB2GRAY);
+  std::cout << (gil::vec3f{2.0, 0.0, 0.0} < gil::vec3f{1.0, 3.0, 0.0}) <<std::endl;
+
+  gil::mat<gil::vec3b> water(image_file("water.jpg"));
+  gil::mat<gil::vec3b> dog(image_file("dog.jpg"));
+  gil::mat<uint8_t> mask(image_file("mask.jpg", image_file::kGreyScale));
   
-  //cv::Mat mask =
-  std::cout << cat.size[0] << " " << cat.size[1] << std::endl;
-  std::cout << dog.size[0] << " " << dog.size[1] << std::endl;
-  std::cout << mask.size[0] << " " << mask.size[1] << std::endl;
-
-  //dog(cv::Rect(200, 50, 900, 500)).copyTo(cat(cv::Rect(200, 500, 900, 500)));
+  gil::mat<gil::vec3f> f = water[gil::vec4<size_t>{200, 200, 600, 600}];
+  gil::mat<gil::vec3f> g = dog[gil::vec4<size_t>{300, 600, 600, 600}];
   
-  copy(dog(cv::Rect(200, 0, 600, 600)), mask, cat(cv::Rect(200, 400, 600, 600)));
-  auto f = cat(cv::Rect(200, 400, 600, 600));
-  auto g = dog(cv::Rect(200, 0, 600, 600));
+  gil::mat<gil::vec3f> b = make_guidance2(f, g, mask, make_boundary(mask));
+  apply_mask(mask, gil::mat_view<gil::vec3f>(b));
+  apply_mask(mask, gil::mat_view<gil::vec3f>(g));
+  f = gil::vec3f();
+  for (int i = 0; i < 200; ++i) {
+    jacobi_iteration(g, b, mask, f);
+    g.swap(f);
+  }
 
-  cv::namedWindow("Display window", cv::WINDOW_NORMAL); // Create a window for display.
-  cv::resize(cat, cat, cv::Size(613, 400));
-  cv::imshow("Display window", make_target(f, g, mask, make_boundary(mask)));  // Show our image inside it.
-  cv::resizeWindow("Display window", 613, 400);
+  f = water[gil::vec4<size_t>{200, 200, 600, 600}];
+  copy(g, mask, f);
+  //cat[gil::vec4<size_t>{200, 2000, 600, 600}] = gil::mat_view<gil::vec3f>(f);
+  
+  /*gil::mat<gil::vec3f> tmp(g.size());
+  apply_mask(mask, gil::mat_view<gil::vec3f>(g));
+  apply_remainder(g, mask, tmp);
+  g.apply(std::bind(acier::multiplies(), _1, 4.0));
+  g -= tmp;
+  g -= b;*/
+  
+  //f -= b;
 
-  cv::waitKey(0); // Wait for a keystroke in the window
+  f.apply(std::bind(acier::divides(), _1, 255.0));
+  display<gil::vec3f>(f, {600, 600});
+  
+
+  //display(mask);
   return 0;
 }
