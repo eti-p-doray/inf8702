b9c5a417fee840a64061be7c1105dff1f7681c1e
Bettorun <youri.barsalou@gmail.com>
2017-11-27 23:55:55 -0500

    Implementing TBB parallel jacobi iteration

--- poisson_tbb.cpp
+++ poisson_tbb.cpp
@@ -32,10 +32,6 @@ public:
     }
   }
 
-  gil::mat_view<uint8_t> getBoundary() const {
-    return boundary_;
-  }
-
 private:
   gil::mat_cview<uint8_t> mask_;
   size_t mask_step_;
@@ -48,7 +44,7 @@ gil::mat<uint8_t> tbb_make_boundary(gil::mat_cview<uint8_t> mask) {
   gil::mat<uint8_t> boundary({mask.rows(), mask.cols()});
   ParallelBoundary para_bound(mask, boundary);
   parallel_for(blocked_range<size_t>(0, mask.rows() - 1), para_bound);
-  return para_bound.getBoundary();
+  return boundary;
 }
 
 /**
@@ -70,7 +66,7 @@ public:
     const gil::mat_cview<uint8_t>& mask, const gil::mat_cview<uint8_t>& boundary,
     gil::mat_view<gil::vec3f>& guidance)
     : mask_(mask), boundary_(boundary), g_(g), f_(f), guidance_(guidance),
-      f_step_(f_.stride()), g_step_(g.stride()), bound_step_(boundary.stride()){
+      f_step_(f_.stride()), g_step_(g_.stride()), bound_step_(boundary_.stride()){
     // empty, all in initialisation list
   }
 
@@ -214,7 +210,43 @@ gil::mat<gil::vec3f> tbb_make_guidance_mixed_gradient(gil::mat_cview<gil::vec3f>
   return dst;
 }
 
-//for
+/**
+ * Class used by tbb to apply the parallel_for calculating the Jacobi iteration
+ */
+class ParallelJacobi {
+public:
+  ParallelJacobi(const gil::mat_cview<gil::vec3f>& src, const gil::mat_cview<gil::vec3f>& b,
+    const gil::mat_cview<uint8_t>& mask, gil::mat_view<gil::vec3f>& dst)
+    : src_(src), b_(b), mask_(mask), dst_(dst), src_step_(src_.stride()) {
+    //empty, all in initialisation list
+  }
+
+  void operator()(const blocked_range<size_t>& range) const {
+    for (size_t i = range.begin(); i != range.end(); ++i) {
+      const gil::vec3f* src_it = src_.row_cbegin(i)+1;
+      const gil::vec3f* b_it = b_.row_cbegin(i)+1;
+      const uint8_t* mask_it = mask_.row_cbegin(i)+1;
+      gil::vec3f* dst_it = dst_.row_begin(i)+1;
+      for (size_t j = 1; j < src_.cols()-1; ++j, ++src_it, ++dst_it, ++b_it, ++mask_it) {
+        if (*mask_it >= 128) { // if part of the region targeted by the mask
+          // Find f_p's next value. This is basically the equation 7 presented
+          // in http://www.cs.virginia.edu/~connelly/class/2014/comp_photo/proj2/poisson.pdf
+          // except the 2nd term of the left side was added on both side, and both
+          // sides were devided by |Np| (4.0). Therefore, boundary + f's value for each
+          // neighboors, devided by 4
+          *dst_it = (*b_it + src_it[-1] + src_it[1] + src_it[-src_step_] + src_it[src_step_]) / 4.0;
+        }
+      }
+    }
+  }
+
+private:
+  gil::mat_view<gil::vec3f> dst_;
+  gil::mat_cview<gil::vec3f> src_;
+  gil::mat_cview<gil::vec3f> b_;
+  gil::mat_cview<uint8_t> mask_;
+  size_t src_step_;
+};
 /**
  * Function to execute one iteration of the iterative Jacobi method, applied to
  * the poisson equation. It calculates the left side of the equation and finds
@@ -228,23 +260,8 @@ void tbb_jacobi_iteration(gil::mat_cview<gil::vec3f> src,
   assert(src.size() == mask.size());
   assert(b.size() == mask.size());
   assert(dst.size() == mask.size());
-  size_t src_step = src.stride();
-  for (int i = 1; i < src.rows()-1; ++i) {
-    const gil::vec3f* src_it = src.row_cbegin(i)+1;
-    const gil::vec3f* b_it = b.row_cbegin(i)+1;
-    const uint8_t* mask_it = mask.row_cbegin(i)+1;
-    gil::vec3f* dst_it = dst.row_begin(i)+1;
-    for (size_t j = 1; j < src.cols()-1; ++j, ++src_it, ++dst_it, ++b_it, ++mask_it) {
-      if (*mask_it >= 128) { // if part of the region targeted by the mask
-        // Find f_p's next value. This is basically the equation 7 presented
-        // in http://www.cs.virginia.edu/~connelly/class/2014/comp_photo/proj2/poisson.pdf
-        // except the 2nd term of the left side was added on both side, and both
-        // sides were devided by |Np| (4.0). Therefore, boundary + f's value for each
-        // neighboors, devided by 4
-        *dst_it = (*b_it + src_it[-1] + src_it[1] + src_it[-src_step] + src_it[src_step]) / 4.0;
-      }
-    }
-  }
+  ParallelJacobi para_jacobi(src, b, mask, dst);
+  parallel_for(blocked_range<size_t>(1, src.rows()-1), para_jacobi);
 }
 
 //for
